1f0fa278f9d841d345e8a8aeb2998daed8bc717c
sandeep-garai <79070318+sandeep-garai@users.noreply.github.com>
2025-05-01 00:42:38 +0530

    Time: 2 ms (56.57%), Space: 7.9 MB (38.32%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -3,6 +3,8 @@
 
 class Solution {
 public:
+    //v2 using binary search (for sorted array(versions))
+    //as the all the version after bad version is bad; finding first bad version
     int firstBadVersion(int n) {
         int l=1,r=n;
         while(l<r){
