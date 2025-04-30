f29e02b017b75e152db4c4bfff2ff32a87467049
sandeep-garai <79070318+sandeep-garai@users.noreply.github.com>
2025-05-01 00:34:23 +0530

    Time: 2 ms (56.57%), Space: 7.9 MB (68.96%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,9 +4,12 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        for(int i=1 ;i<=n;i++){
-            if(isBadVersion(i)) return i;
+        int l=1,r=n;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(isBadVersion(mid))   r=mid;
+            else    l=mid+1;
         }
-        return n+1;
+        return l;
     }
 };
