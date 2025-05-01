0159bac778ef2faadb88db8189a145bec422b101
sandeep-garai <79070318+sandeep-garai@users.noreply.github.com>
2025-05-01 16:03:34 +0530

    Time: 50 ms (53.06%), Space: 51 MB (24%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     //O(n^2) solution
     int subarraySum(vector<int>& nums, int k) {
-        int cnt=0;
-        int len=nums.size();
-        for(int i=0;i<len;i++){
-            int sum=0;
-            for(int j=i;j<len;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    cnt++;
-                }
-            }
+        int preSum=0,ans=0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            preSum+=nums[i];
+            int remove=preSum-k;
+            ans+=mp[remove];
+            mp[preSum]++;
         }
-        return cnt;
+        return ans;
     }
 };
