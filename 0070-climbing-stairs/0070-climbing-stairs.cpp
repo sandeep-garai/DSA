52b974dabc3e3791126cbd56e878cd1506eca68d
sandeep-garai <79070318+sandeep-garai@users.noreply.github.com>
2025-06-08 20:05:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    //memoization technique
-    int climb(int n,unordered_map<int,int>&memo){
-        if(n==0 || n==1)   
-            return 1;
-        if(memo.find(n)==memo.end()){
-            memo[n] = climb(n-1,memo) + climb(n-2,memo);
-        }
-        return memo[n];
-    }
-    
+//tabulation 
     int climbStairs(int n) {
-        unordered_map<int,int> memo;
-        return climb(n,memo);
+        if(n==0 || n==1)    return 1;
+
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+
+        for(int i=2;i<=n ; i++){
+            dp[i] = dp[i-1] + dp [i-2];
+        }
+        return dp[n];
     }
 };
